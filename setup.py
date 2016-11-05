5555e13820c16e2c67e5c82689bd907e02bf1064
Rabir <snake2n@yahoo.fr>
2016-11-06 00:13:55 +0100

    Setup update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='pyAqara',
-      version='1.0',
+      version='1.1',
       zip_safe=False,
       platforms=["any"],
       packages=find_packages(),
